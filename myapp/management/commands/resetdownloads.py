c9137f834587205e6aa2f37b46165915c1120899
javadsadeqiii <151460378+javadsadeqiii@users.noreply.github.com>
2024-01-14 13:30:18 +0330

    customAvatar

--- myapp/management/commands/resetdownloads.py
+++ myapp/management/commands/resetdownloads.py
@@ -4,15 +4,15 @@ from myapp.models import CustomUser
 
 
 
-class Command(BaseCommand):
+#class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-        thirty_days_ago = timezone.now() - timezone.timedelta(days=30)
-        users_to_reset = CustomUser.objects.filter(last_download_date__lte=thirty_days_ago)
+  #  def handle(self, *args, **options):
+    #    thirty_days_ago = timezone.now() - timezone.timedelta(days=30)
+    #    users_to_reset = CustomUser.objects.filter(last_download_date__lte=thirty_days_ago)
 
-        for user in users_to_reset:
-            user.downloads = 5  
-            user.save()
+    #    for user in users_to_reset:
+    #        user.downloads = 5  
+    #        user.save()
 
-        self.stdout.write(self.style.SUCCESS('تعداد دانلودهای مجاز با موفقیت بروزرسانی شد'))
+   #     self.stdout.write(self.style.SUCCESS('تعداد دانلودهای مجاز با موفقیت بروزرسانی شد'))
         
